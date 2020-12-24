7d61c339ffe050a36ee455e9209b7201324e89b6
GarfieldZHU <Garfield.bupt@gmail.com>
2020-12-24 17:11:04 +0800

    feat: add frame

---
+++ src/frame.rs
@@ -0,0 +1,11 @@
+use bytes::{Buf, Bytes};
+
+#[derive(Clone, Debug)]
+pub enum Frame {
+    Simple(String),
+    Error(String),
+    Integer(u64),
+    Bulk(Bytes),
+    Null,
+    Array(Vec<Frame>),
+}
