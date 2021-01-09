9a0b157965c32801061e71d6bd4f325e98feb9de
GarfieldZHU <Garfield.bupt@gmail.com>
2021-01-09 22:36:01 +0800

    feat: add frame connection

---
+++ src/connection.rs
@@ -0,0 +1,53 @@
+use bytes::{Buf, BytesMut};
+use tokio::net::TcpStream;
+use tokio::io::AsyncReadExt;
+use mini_redis::Result;
+
+pub struct Connection {
+    stream: TcpStream,
+    buffer: Vec<u8>,
+    cursor: usize,
+}
+
+impl Connection {
+    pub fn new(stream: TcpStream) -> Connection {
+        Connection {
+            stream,
+            // Allocate the buffer with 4kb of capacity.
+            buffer: vec![0; 4096],
+            cursor: 0,
+        }
+    }
+
+    pub async fn read_frame(&mut self) -> Result<Option<Frame>> {
+        loop {
+            if let Some(frame) = self.parse_frame()? {
+                return Ok(Some(frame));
+            }
+
+            // Ensure the buffer has capacity
+            if self.buffer.len() == self.cursor {
+                // Grow the buffer
+                self.buffer.resize(self.cursor * 2, 0);
+            }
+
+            // Read into the buffer, tracking the number
+            // of bytes read
+            let n = self.stream.read(
+                &mut self.buffer[self.cursor..]).await?;
+
+            if 0 == n {
+                if self.cursor == 0 {
+                    return Ok(None);
+                } else {
+                    return Err("connection reset by peer".into());
+                }
+            } else {
+                // Update our cursor
+                self.cursor += n;
+            }
+        }
+    }
+}
+
+
